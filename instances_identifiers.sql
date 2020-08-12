1fd10159e830da89abb345c7e33db9e19641dece
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-08-11 22:59:56 -0400

    First commit

---
+++ instances_identifiers.sql
@@ -0,0 +1,20 @@
+BEGIN;
+
+CREATE TABLE local.instances_identifiers AS
+    SELECT id AS instances_id,
+           json_extract_path_text(
+               json_array_elements(json_extract_path(data, 'identifiers')),
+                                   'identifierTypeId' ) AS type_id,
+           json_extract_path_text(
+               json_array_elements(json_extract_path(data, 'identifiers')),
+                                   'value' ) AS value
+        FROM inventory_instances;
+
+CREATE INDEX ON local.instances_identifiers (instances_id);
+CREATE INDEX ON local.instances_identifiers (type_id);
+CREATE INDEX ON local.instances_identifiers (value);
+
+COMMIT;
+
+VACUUM local.instances_identifiers;
+ANALYZE local.instances_identifiers;
---
+++ lookup_oclc.sql
@@ -0,0 +1,11 @@
+SELECT oclc_numbers.oclc_number,
+       instances_identifiers.instances_id,
+       inventory_instances.hrid,
+       inventory_instances.title,
+       inventory_instances.discovery_suppress
+    FROM local.oclc_numbers
+        JOIN local.instances_identifiers
+            ON type_id = '439bfbae-75bc-4f74-9fc7-b2a2d47ce3ef' AND
+	       oclc_numbers.oclc_number = instances_identifiers.value
+	JOIN inventory_instances
+	    ON instances_identifiers.instances_id = inventory_instances.id;
